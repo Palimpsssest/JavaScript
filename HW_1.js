5f83b169ccdd592bacb91492dd9189d07add3f39
Palimpsssest <106396332+Palimpsssest@users.noreply.github.com>
2022-06-20 01:51:39 +0600

    Update HW_1.js

--- HW_1.js
+++ HW_1.js
@@ -94,7 +94,7 @@ let age_3 = 60;
 //  33. Иначе выводите “Technical work”.
 
 if (age_1 < age_2) {
-    console.log('You don’t have access cause your age is ' + age_1 + ' It’s less then ' + age_2);
+    console.log(`You don’t have access cause your age is ${age} It’s less then 18`);
 } else if (age_1 >= age_2 && age_1 < age_3) {
     console.log('Welcome!');
 } else if (age_1 > age_3) {
